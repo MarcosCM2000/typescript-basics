e671312f6e9b98ad36caf0fdacff5beaf549146a
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-10-15 12:59:17 -0600

    array types

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-var person = {
+/*const person: {
+    name: string,
+    age: number
+}*/ var person = {
     name: 'Marcos',
-    age: 20
+    age: 20,
+    hobbies: ['sports', 'cooking']
 };
 console.log(person);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- app.ts
+++ app.ts
@@ -1,8 +1,12 @@
-const person: {
+/*const person: {
     name: string,
     age: number
-}  = {
+}*/ const person  = {
     name: 'Marcos',
-    age: 20
+    age: 20,
+    hobbies: ['sports', 'cooking']
 }
 console.log(person);
+for (const hobby of person.hobbies) {
+    console.log(hobby);
+}
