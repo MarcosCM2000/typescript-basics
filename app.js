ae8cd2f5691c28ed0db1ecc33ced9d08a609419a
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-10-15 13:17:36 -0600

    enum

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 /*const person: {
     name: string,
-    age: number
-}*/ var person = {
+    age: number,
+    hobbies: string[],
+    role: [number, string]
+}*/
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
+var person = {
     name: 'Marcos',
     age: 20,
-    hobbies: ['sports', 'cooking']
+    hobbies: ['sports', 'cooking'],
+    role: Role.ADMIN
 };
 console.log(person);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
     console.log(hobby);
+}
+if (person.role === Role.ADMIN) {
+    console.log('xxxx');
 }
--- app.ts
+++ app.ts
@@ -1,15 +1,20 @@
-const person: {
+/*const person: {
     name: string,
     age: number,
     hobbies: string[],
     role: [number, string]
-} = {
+}*/ 
+enum Role {ADMIN, READ_ONLY, AUTHOR};
+const person = {
     name: 'Marcos',
     age: 20,
     hobbies: ['sports', 'cooking'],
-    role: [1 , '1']
+    role: Role.ADMIN
 }
 console.log(person);
 for (const hobby of person.hobbies) {
     console.log(hobby);
+}
+if (person.role === Role.ADMIN) {
+    console.log('xxxx')
 }
