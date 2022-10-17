3a33e1100312a9f3a8a7ed303bbec495d6215f09
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-10-16 20:26:20 -0600

    literal types

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function combine(n1, n2) {
+function combine(n1, n2, resultConversion) {
     if (typeof n1 === 'number' && typeof n2 === 'number') {
         return n1 + n2;
     }
@@ -6,5 +6,5 @@ function combine(n1, n2) {
         return n1.toString() + n2.toString();
     }
 }
-console.log(combine(20, 25));
-console.log(combine('20', '25'));
+console.log(combine(20, 25, 'as-number'));
+console.log(combine('20', '25', 'as-text'));
--- app.ts
+++ app.ts
@@ -1,4 +1,4 @@
-function combine(n1 :number | string, n2:number | string) {
+function combine(n1 :number | string, n2:number | string, resultConversion: 'as-number'|'as-text') {
     if (typeof n1 === 'number' && typeof n2 === 'number') {
         return n1+n2;
     } else {
@@ -6,5 +6,5 @@ function combine(n1 :number | string, n2:number | string) {
     }
 }
 
-console.log(combine(20,25));
-console.log(combine('20','25'));
+console.log(combine(20,25, 'as-number'));
+console.log(combine('20','25', 'as-text'));
