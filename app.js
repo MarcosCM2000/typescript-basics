66abcd584eb0dcefe461b092338872415b404616
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-10-16 21:34:38 -0600

    unknown & never

--- app.js
+++ app.js
@@ -1,32 +1,13 @@
-function combine(n1, n2, resultConversion) {
-    if (typeof n1 === 'number' && typeof n2 === 'number') {
-        return n1 + n2;
-    }
-    else {
-        return n1.toString() + n2.toString();
-    }
+//  unknown
+var userInput;
+var userName;
+userInput = 5;
+userName = 'Max';
+//  never
+function generateError(msg, code) {
+    throw {
+        message: msg,
+        codeNumber: code
+    };
 }
-//  literal types
-console.log(combine(20, 25, 'as-number'));
-console.log(combine('20', '25', 'as-text'));
-//  function as types
-function printResult(n) {
-    console.log(n);
-}
-var combineValues;
-combineValues = printResult;
-console.log(combineValues('s'));
-//  function types & callbacks
-function addAndHandle(n1, n2, cb) {
-    var result = n1 + n2;
-    cb(result);
-}
-addAndHandle(10, 20, function () { });
-function sendRequest(data, cb) {
-    // ... sending a request with "data"
-    return cb({ data: 'Hi there!' });
-}
-sendRequest('Send this!', function (response) {
-    console.log(response);
-    return true;
-});
+generateError('An error occured', 500);
--- app.ts
+++ app.ts
@@ -1,41 +1,14 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number'|'as-text';
-function combine(
-    n1  :Combinable,
-    n2  :Combinable,
-    resultConversion: ConversionDescriptor) {
-    if (typeof n1 === 'number' && typeof n2 === 'number') {
-        return n1+n2;
-    } else {
-        return n1.toString() + n2.toString();
-    }
+//  unknown
+let userInput: unknown;
+let userName: string;
+userInput = 5;
+userName = 'Max';
+
+//  never
+function generateError(msg: string, code: number): never{
+    throw{
+        message: msg,
+        codeNumber: code
+    };
 }
-//  literal types
-console.log(combine(20,25, 'as-number'));
-console.log(combine('20','25', 'as-text'));
-
-//  function as types
-function printResult(n: string): void {
-    console.log(n)
-}
-let combineValues: (x: string)=> void;
-combineValues = printResult;
-
-console.log(combineValues('s'))
-
-//  function types & callbacks
-function addAndHandle(n1: number, n2: number, cb: (num: number)=>void){
-    const result = n1+n2;
-    cb(result);
-}
-addAndHandle(10,20,()=>{})
-
-function sendRequest(data: string, cb: (response: any) => void) {
-    // ... sending a request with "data"
-    return cb({data: 'Hi there!'});
-  }
-   
-  sendRequest('Send this!', (response) => { 
-    console.log(response);
-    return true;
-});
+generateError('An error occured', 500);
