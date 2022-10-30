a8331512c28841d818dab210eb30b900889597c9
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-10-29 20:39:46 -0600

    array & object destructuring

--- dist/app.js
+++ dist/app.js
@@ -11,4 +11,22 @@ function generateError(msg, code) {
         codeNumber: code
     };
 }
-generateError('An error occured', 500);
+//generateError('An error occured', 500);
+const hobbies = ['sport', 'hiking'];
+//  spread operator
+const person = {
+    name: 'max',
+    age: 30
+};
+const copiedPerson = Object.assign({}, person);
+//  rest parameters
+const add2 = (...numbers) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+const addedNumbers = add2(5, 10, 3, 2.7); //20.7
+//  array destructuring
+const [] = hobbies;
+//  object destructuring
+const {} = person;
--- src/app.ts
+++ src/app.ts
@@ -11,4 +11,25 @@ function generateError(msg: string, code: number): never{
         codeNumber: code
     };
 }
-generateError('An error occured', 500);
+//generateError('An error occured', 500);
+
+const hobbies = ['sport', 'hiking']
+//  spread operator
+const person = {
+    name: 'max',
+    age: 30
+}
+const copiedPerson = { ...person };
+
+//  rest parameters
+const add2 = (...numbers: number[]) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue
+    }, 0);
+};
+const addedNumbers = add2(5,10,3,2.7) //20.7
+
+//  array destructuring
+const [] = hobbies;
+//  object destructuring
+const {} = person;
