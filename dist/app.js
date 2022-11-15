d3a5f1cfe7a6f352f2acb51853e3e4684bf0e400
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-11-14 19:17:36 -0600

    public and private access modifiers

--- dist/app.js
+++ dist/app.js
@@ -2,13 +2,21 @@
 class Department {
     // executed when object is created 
     constructor(n) {
+        this.employees = [];
         this.name = n;
     }
     describe() {
         console.log('Department: ' + this.name);
     }
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeeInfo() {
+        console.log(this.employees);
+    }
 }
 const accounting = new Department('Accounting');
 accounting.describe();
-const accountingCopy = { name: 's', describe: accounting.describe }; // pointer at the described method in my accounting object
-accountingCopy.describe();
+accounting.addEmployee('Marcos');
+//  const accountingCopy = { name: 's', describe: accounting.describe } // pointer at the described method in my accounting object
+//  accountingCopy.describe();
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-    name: string;   //  field of class | property
+    public name: string;   //  field of class | property
+    private employees: string [] = [];
 
     // executed when object is created 
     constructor(n: string){
@@ -9,10 +10,18 @@ class Department {
     describe(this: Department ) { //to always refer to an instance that's based on department class
         console.log('Department: ' + this.name ); 
     }
+
+    addEmployee(employee: string) {
+        this.employees.push(employee);
+    }
+    printEmployeeInfo(){
+        console.log(this.employees);
+    }
 }
 
 const accounting = new Department('Accounting');
-accounting.describe();
+    accounting.describe();
+    accounting.addEmployee('Marcos');
 
-const accountingCopy = { name: 's', describe: accounting.describe } // pointer at the described method in my accounting object
-accountingCopy.describe();
+//  const accountingCopy = { name: 's', describe: accounting.describe } // pointer at the described method in my accounting object
+//  accountingCopy.describe();
