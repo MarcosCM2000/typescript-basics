9a8dff1c17d9482852d56c390314de6c534c94ae
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-11-16 14:50:42 -0600

    generic classes

--- dist/app.js
+++ dist/app.js
@@ -28,3 +28,20 @@ function extractAndConvert(obj, key) {
     return 'Value: ' + obj[key];
 }
 extractAndConvert({ name: 'Marcos' }, 'name');
+//  Generic classes
+class DataStorage {
+    constructor() {
+        this.data = [];
+    }
+    addItem(item) {
+        this.data.push(item);
+    }
+    removeItem(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    getItems() {
+        return [...this.data];
+    }
+}
+const textStorage = new DataStorage();
+const numberStorage = new DataStorage();
--- src/app.ts
+++ src/app.ts
@@ -38,3 +38,20 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 }
 
 extractAndConvert({name: 'Marcos'}, 'name');
+
+//  Generic classes
+class DataStorage<T extends string | number | boolean> {
+    private data: T[] = [];
+    addItem(item: T){
+        this.data.push(item);
+    }
+    removeItem(item:T){
+        this.data.splice(this.data.indexOf(item),1);
+    }
+    getItems(){
+        return [...this.data];
+    }
+}
+
+const textStorage = new DataStorage<string>();
+const numberStorage = new DataStorage<number>();
