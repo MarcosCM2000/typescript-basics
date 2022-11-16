74dbb69c17526b478bc10fb625575b299ca24f58
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-11-16 14:28:10 -0600

    constraints

--- dist/app.js
+++ dist/app.js
@@ -12,4 +12,6 @@ function merge(objA, objB) {
     return Object.assign(Object.assign({}, objA), objB);
 }
 const mergedObj = merge({ name: 'Marcos' }, { age: 22 });
+const mergedObj2 = merge({ x: 2 }, { y: 4 });
 console.log(mergedObj.age);
+console.log(mergedObj2);
--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,11 @@ const promise: Promise<string> = new Promise((resolve, reject) => {
 promise.then(data => {}, failure => {})
 
 //  generic functions
-function merge<T, U>(objA: T, objB: U){
+function merge<T extends object, U extends object>(objA: T, objB: U){
     return {...objA, ...objB};
 }
 
-const mergedObj = merge({name: 'Marcos'}, {age: 22})
+const mergedObj = merge({name: 'Marcos'}, {age: 22});
+const mergedObj2 = merge({x: 2}, {y: 4});
 console.log(mergedObj.age);
+console.log(mergedObj2);
