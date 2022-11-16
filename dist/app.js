0ccd9dd88f071fd8522640db36eef99840bf749c
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-11-16 12:58:54 -0600

    generics

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,8 @@
 "use strict";
+const names = [];
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('This is done');
+    }, 2000);
+});
+promise.then(data => { }, failure => { });
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+const names: Array<string> = [];
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        resolve('This is done')
+    }, 2000)
+})
+
+promise.then(data => {}, failure => {})
