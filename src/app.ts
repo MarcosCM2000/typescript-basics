2401729e353a9fe5cc0ad57e00df69363b2e565b
MarcosCInland <marcos.andres@inlandlogistics.co>
2022-12-20 22:26:24 -0600

    accessors and parameters decorators

--- src/app.ts
+++ src/app.ts
@@ -34,11 +34,33 @@ function Log(target: any, propertyName: string){
     console.log('Property decorator');
     console.log(target, propertyName);
 }
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor){
+    console.log('Accesor decorator');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log3(target: any, name: string | symbol, descriptor: PropertyDescriptor){
+    console.log('Method decorator');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log4(target: any, name: string | symbol, position: number){
+    console.log('Parameter decorator');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
 class Product {
     @Log
     title: string;
     private _price: number;
 
+    @Log2
     set price (val: number){
         if (val > 0) {
             this._price = val;
@@ -52,7 +74,8 @@ class Product {
         this._price = p;
     }
 
-    getPriceWithTax(tax: number){
+    @Log3
+    getPriceWithTax(@Log4 tax: number){
         return this._price * (1 + tax);
     }
 }
